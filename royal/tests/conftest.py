1b583659e36e4569bd28f7cb2a14ea67f80f0ec1
Éric Araujo <earaujo@caravan.coop>
2017-10-12 00:22:39 -0400

    convert nose setup to pytest

---
+++ royal/tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+
+import logging
+
+import pytest
+from alembic import command
+from alembic.config import Config
+from sqlalchemy import create_engine
+from sqlalchemy.schema import DropConstraint, MetaData
+
+
+log = logging.getLogger(__name__)
+
+
+mysql_uri = 'mysql+mysqlconnector://root@localhost/royal_test_example'
+engine = None
+
+
+@pytest.fixture(autouse=True, scope='session')
+def setup():
+    os.environ['MONGO_URI'] = 'mongodb://localhost'
+    os.environ['MONGO_DB_NAME'] = 'royal_example'
+    os.environ['MONGO_DB_PREFIX'] = ''
+
+    # sqla extension setup
+    global engine
+
+    alembic_config = Config()
+    alembic_config.set_main_option('script_location', 'example/ext/sqla/db')
+    alembic_config.set_main_option('sqlalchemy.url', mysql_uri)
+
+    engine = create_engine(mysql_uri)
+
+    try:
+        command.downgrade(alembic_config, 'base')
+    except:
+        log.exception("Migration downgrade failed, clearing all tables")
+        metadata = MetaData(engine)
+        metadata.reflect()
+        for table in metadata.tables.values():
+            for fk in table.foreign_keys:
+                engine.execute(DropConstraint(fk.constraint))
+        metadata.drop_all()
+
+    command.upgrade(alembic_config, 'head')
